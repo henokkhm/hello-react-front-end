b84c31ffa635dd1c29bc8d16d8143b40026f013a
Henok Kirubel <henokhm2@gmail.com>
2024-02-03 23:16:07 +0300

    Add loading and error states to Greeting component

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,7 +1,39 @@
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { fetchGreeting } from '../redux/features/greetingSlice';
 
 function Greeting() {
-  const { phrase, language } = useSelector((state) => state.greeting);
+  const {
+    phrase,
+    language,
+    isLoading,
+    isError,
+  } = useSelector(
+    (state) => state.greeting,
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, [dispatch]);
+
+  const getRandomGreeting = () => {
+    dispatch(fetchGreeting());
+  };
+
+  if (isLoading) {
+    return <div className="wrapper">Loading...</div>;
+  }
+
+  if (isError) {
+    return (
+      <div className="wrapper">
+        Something went wrong. Please try reloading the page...
+      </div>
+    );
+  }
 
   return (
     <div className="wrapper">
@@ -10,6 +42,9 @@ function Greeting() {
         <span className="language_name">{language}</span>
       </p>
       <div className="greeting">{phrase}</div>
+      <button type="button" className="btn" onClick={getRandomGreeting}>
+        Randomize
+      </button>
     </div>
   );
 }
