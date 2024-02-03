e195952d7964a2ceecd93cc2c611ba2110d624f4
Henok Kirubel <henokhm2@gmail.com>
2024-02-03 23:00:47 +0300

    Display greeting from redux state in Greeting component

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,5 +1,17 @@
+import { useSelector } from 'react-redux';
+
 function Greeting() {
-  return <div>Greeting</div>;
+  const { phrase, language } = useSelector(
+    (state) => state.greeting,
+  );
+
+  return (
+    <div>
+      <span>{phrase}</span>
+      <span> is &quot;Hello&quot; in </span>
+      <span>{language}</span>
+    </div>
+  );
 }
 
 export default Greeting;
