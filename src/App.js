43f0129c2cfac5ef85c1e3377354c5fc16264c2b
Henok Kirubel <henokhm2@gmail.com>
2024-02-03 22:40:33 +0300

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+
+import Greeting from './components/Greeting';
+
 function App() {
   return (
-    <div>Hello Microverse</div>
+    <main>
+      <div>Hello Microverse</div>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </main>
   );
 }
 
---
+++ src/components/Greeting.js
@@ -0,0 +1,5 @@
+function Greeting() {
+  return <div>Greeting</div>;
+}
+
+export default Greeting;
